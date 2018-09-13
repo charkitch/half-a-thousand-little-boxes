cc1209e0852688b24143ea597e0f687aa0fd384a
charkitch <charles.kitchen@gmail.com>
2018-09-13 09:50:36 -0400

    get buttons on splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const splash = () => (
-  <div className="splash-page">
-    <img src={window.splash} />
-    <h1>Get inspired and share your best photos</h1>
-    <h2>Find your home among the world's best photographers</h2>
+  <div className="splash-container">
+    <img src={window.splash} className="splash-image" alt='A bear walking into a chasm along a shore in Alaska.'/>
+    <h1 className="splash-center-appeal-headline">Get inspired and share your best photos</h1>
+    <h2 className="splash-center-appeal-sub">Find your home among the world's best photographers</h2>
+    <Link className="splash-center-appeal-join" to="signup">Join HATLB</Link>
   </div>
 );
 
