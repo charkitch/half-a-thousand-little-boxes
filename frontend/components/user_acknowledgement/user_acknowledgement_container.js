e1bbb01b88cfcf6f8e56dcde655b5c148a6ea61c
charkitch <charles.kitchen@gmail.com>
2018-09-18 09:01:17 -0400

    fix require tree

--- frontend/components/user_acknowledgement/user_acknowledgement_container.js
+++ frontend/components/user_acknowledgement/user_acknowledgement_container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
-import { sendPhoto } from '../../actions/frames_actions';
+import { createFrame } from '../../actions/frames_actions';
 import UserAcknowledgement from './user_acknowledgement';
 import { openModal } from '../../actions/modal_actions';
 
@@ -8,7 +8,7 @@ const mapDispatchToProps = dispatch => {
   return {
     logout: () => dispatch(logout()),
     openModal: (modal) => dispatch(openModal(modal)),
-    sendPhoto: (frame) => dispatch(sendPhoto(frame)),
+    createFrame: (frame) => dispatch(createFrame(frame)),
   };
 };
 
